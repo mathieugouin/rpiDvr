828b8422b5a3aedf489d69127ae89db58f2ae195
Mathieu Gouin <mgouin@gmail.com>
2017-11-12 10:53:11 -0500

    Update cleanIconsCache.sh
    Added content

--- zap2xml/cleanIconsCache.sh
+++ zap2xml/cleanIconsCache.sh
@@ -1,1 +1,11 @@
+#! /bin/bash
+################################################################################
+# When changing tvheadend channel icons, it is required to clean the cache to
+# force Kodi to reload the new icons.
+################################################################################
 
+rm $HOME/.kodi/userdata/Thumbnails/*/*.png
+rm $HOME/.kodi/userdata/Database/Textures13.db
+
+echo Don\'t forget to reboot:
+echo sudo reboot
