9ecb6ade665bdb4d7ff0fcf72b87efbcf374246f
Mathieu Gouin <mgouin@gmail.com>
2021-05-13 21:26:47 -0400

    Clean-up

--- tools/autorec_list.py
+++ tools/autorec_list.py
@@ -40,10 +40,6 @@ def find_files(directory, pattern):
                 yield filename
 
 
-def test():
-    pass
-
-
 def get_channel_dict():
     d = {}
     for f in find_files(TVH_CHANNEL_DIR, TVH_FILE_PATTERN):
@@ -65,12 +61,6 @@ def process():
     #print ch_dict
 
     # Get all autorecs
-    """
-    "title": "District 31", 
-    "channel": "52cf67a489e79fdb75b9081e5ee8865e", 
-    "name": "District 31", 
-    "directory": "District-31", 
-    """
     ar = []
     for f in find_files(TVH_AUTOREC_DIR, TVH_FILE_PATTERN):
         #print f
@@ -90,46 +80,15 @@ def process():
                 ar_dict['channel'] = ''
             ar.append(ar_dict)
 
-    #for a in ar:
-    #    print a
-
     df = pd.DataFrame(ar)
     df.sort_values('channel', inplace=True)
     # No wrap when printing
     pd.set_option('display.expand_frame_repr', False)
     print df
 
-    return
-
-    existing_recording_logs = [os.path.relpath(f, TVH_DVR_DIR) for f in find_files(TVH_DVR_DIR, TVH_FILE_PATTERN)]
-
-    for video_file in find_files(VIDEO_DIR, "*.ts"):
-        print "Processing", video_file
-
-        video_directory = get_video_directory(video_file)
-
-        log_name = get_new_filename()
-        # Generate a new name in case already present (low probability)
-        while log_name in existing_recording_logs:
-            log_name = get_new_filename()
-
-        # getmtime return in seconds, round down to a decent resolution
-        video_mod_time = roundup(os.path.getmtime(video_file), START_TIME_RESOLUTION)
-        video_duration = roundup(get_video_duration(video_file), DURATION_RESOLUTION)
-
-        title = os.path.splitext(os.path.basename(video_file))[0]
-
-        outFileName = os.path.join(TVH_DVR_DIR, log_name)  # real
-
-        with open(outFileName, 'wb') as fh_log:
-            json.dump(recordingDict, fh_log, indent=4)
-            fh_log.write('\n')
-            print '  => Created:', outFileName
-
 
 def _main():
     process()
-    #test()
 
 
 if __name__ == '__main__':
