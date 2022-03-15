6e864ba27ad0fd4ef5d13df1215dbfead9216b48
Mathieu Gouin <mgouin@gmail.com>
2022-03-14 23:32:25 -0400

    rename & fix autorec limit of 50

--- tools/tvh_reenable_autorecs.py
+++ tools/tvh_reenable_autorecs.py
@@ -20,7 +20,8 @@ def get_api_url(api):
 
 
 def get_autorecs():
-    return get_api_url('dvr/autorec/grid')
+    # Large number to not be limited (default is 50)
+    return get_api_url('dvr/autorec/grid?limit=5000')
 
 
 def get_idnode(uuid):
@@ -41,29 +42,36 @@ def json_pp(js):
     print(json.dumps(js, indent=4, sort_keys=False))
 
 
-def main():
+def _main():
     autorecs = get_autorecs()
     #json_pp(autorecs)
 
     enabled_uuid = []
+    count = 0
 
-    if autorecs and 'entries' in autorecs:
-        for e in autorecs['entries']:
-            if e['enabled']:
-                #print("%s\t%s\t%s" % (e['uuid'], e['enabled'], e['name']))
-                uuid = e['uuid']
-                enabled_uuid.append(uuid)
-                # disable
-                set_autorec_enabled(uuid, False)
+    if autorecs:
+        if 'total' in autorecs:
+            print('Total number of autorecs: %d' % (autorecs['total']))
+        if 'entries' in autorecs:
+            for e in autorecs['entries']:
+                #if True:
+                if e['enabled']:
+                    print("%s\t%s\t%s" % (e['uuid'], e['enabled'], e['name']))
+                    uuid = e['uuid']
+                    enabled_uuid.append(uuid)
+                    # disable
+                    time.sleep(0.2)
+                    set_autorec_enabled(uuid, False)
+                    count += 1
 
-    print("Disabled all enabled autorecs")
-    time.sleep(2)
+    print("Disabled %d enabled autorecs" % (count))
+    time.sleep(1)
 
     for uuid in enabled_uuid:
         print("Enabling " + uuid)
         set_autorec_enabled(uuid, True)
-        time.sleep(0.5)
+        time.sleep(0.2)
 
 
 if __name__ == "__main__":
-    main()
+    _main()
