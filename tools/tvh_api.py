3f1c96def5e1564dfb6ba57021f7da4a56c6b842
Mathieu Gouin <mgouin@gmail.com>
2025-08-21 20:54:55 -0400

    No need for requests

--- tools/tvh_api.py
+++ tools/tvh_api.py
@@ -1,7 +1,7 @@
 # ref: https://github.com/dave-p/TVH-API-docs/wiki
 
 import json
-import requests
+import urllib.request
 import urllib.parse
 
 _TVH_SERVER = 'http://192.168.1.23:9981'
@@ -16,9 +16,24 @@ def get_api_url(api, params=None):
     url = _TVH_SERVER + '/api/' + api
     if params is not None:
         url += "?" + urllib.parse.urlencode(params)
-    ts_response = requests.get(url)
-    if ts_response.status_code != 200:
-        print('Error code %d\n%s' % (ts_response.status_code, ts_response.content))
-        return {}
-    ts_json = json.loads(ts_response.text, strict=False)
-    return ts_json
+    try:
+        with urllib.request.urlopen(url) as response:
+            if response.status != 200:
+                print('Error code %d\n%s' % (response.status, response.read().decode('utf-8')))
+                return {}
+            data = response.read().decode('utf-8')
+            ts_json = json.loads(data, strict=False)
+            return ts_json
+    except urllib.error.HTTPError as e:
+        print('HTTPError: %d\n%s' % (e.code, e.read().decode('utf-8')))
+    except urllib.error.URLError as e:
+        print('URLError:', e.reason)
+    return {}
+
+
+def _main():
+    print(json.dumps(get_api_url('serverinfo'), indent=4))
+
+
+if __name__ == "__main__":
+    _main()
